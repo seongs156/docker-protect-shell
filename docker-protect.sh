574a7ebb38580bfb8aa540386ca82b665a6cd7c6
root <root@localhost.localdomain>
2024-07-01 00:26:52 -0400

    임시저장

--- docker-protect.sh
+++ docker-protect.sh
@@ -23,7 +23,7 @@ fi
 # rm -rf /var/lib/docker
 
 
-### 1. 도커 설치
+# ## 1. 도커 설치
 # if ! command rpm -qa | grep docker-ce &> /dev/null; then
 #     echo "Docker 설치중..."
 #     sudo yum install -y yum-utils
@@ -42,7 +42,7 @@ fi
 
 
 
-### 2. 도커 최신버전 패치
+# ## 2. 도커 최신버전 패치
 # docker_version=$(docker version --format '{{.Server.Version}}')
 # rpm_docker_version=$(rpm -qa | grep docker-ce | awk -F'-' '/docker-ce-[0-9]/ {print $3}')
 
@@ -55,72 +55,79 @@ fi
 
 
 
-### 3. 도커 그룹에 불필요한 사용자 제거
-## 도커그룹 사용자 확인 -> getent group docker
-## 도커그룹 사용자 제거 ->  sudo gpasswd -d soluipx docker
+# ## 3. 도커 그룹에 불필요한 사용자 제거
+# # 도커그룹 사용자 확인 -> getent group docker
+# # 도커그룹 사용자 제거 ->  sudo gpasswd -d soluipx docker
 
 
-# ### 3-1. soluipx 도커그룹 추가
-# if ! command getent group docker | awk -F: '{print $4}' | grep -q "soluipx"; then
-#     sudo usermod -aG docker soluipx
-#     echo "3-1. Docker daemon audit 설정"
-# fi
+### 3-1. soluipx 도커그룹 추가
+if ! command getent group docker | awk -F: '{print $4}' | grep -q "soluipx"; then
+    sudo usermod -aG docker soluipx
+    echo "3-1. Docker daemon audit 설정"
+fi
 
-# ### 3-2. soluipx를 제외한 Docker그룹에 사용자 제거
-# if ! [ "$(getent group docker | awk -F: '{print $4}')" = "soluipx" ]; then
-#     sudo gpasswd -M soluipx docker
-#     echo "3-2. Docker daemon audit 설정"
-# fi
 
-# auditd_flag=false
+### 3-2. soluipx를 제외한 Docker그룹에 사용자 제거
+if ! [ "$(getent group docker | awk -F: '{print $4}')" = "soluipx" ]; then
+    sudo gpasswd -M soluipx docker
+    echo "3-2. Docker daemon audit 설정"
+fi
 
-# ### 4. Docker daemon audit 설정
-# if ! command cat /etc/audit/rules.d/audit.rules | grep /usr/bin/docker &> /dev/null; then
-#     echo "-w /usr/bin/docker" >> /etc/audit/rules.d/audit.rules
-#     echo "4. Docker daemon audit 설정"
-#     auditd_flag=true
-# fi
 
-# ### 5. /var/lib/docker audit 설정
-# if ! command cat /etc/audit/rules.d/audit.rules | grep /var/lib/docker &> /dev/null; then
-#     echo "-w /var/lib/docker -k docker" >> /etc/audit/rules.d/audit.rules
-#     echo "5. /var/lib/docker audit 설정"
-#     auditd_flag=true
-# fi
+### 4. Docker daemon audit 설정
+auditd_flag=false
+if ! command cat /etc/audit/rules.d/audit.rules | grep /usr/bin/docker &> /dev/null; then
+    echo "-w /usr/bin/docker" >> /etc/audit/rules.d/audit.rules
+    echo "4. Docker daemon audit 설정"
+    auditd_flag=true
+fi
 
-# ### 6. /etc/docker audit 설정
-# if ! command cat /etc/audit/rules.d/audit.rules | grep  /etc/docker &> /dev/null; then
-#     echo "-w /etc/docker -k docker" >> /etc/audit/rules.d/audit.rules
-#     echo "6. /etc/docker audit 설정"
-#     auditd_flag=true
-# fi
 
-# ### 7. docker.service audit 설정
-# if ! command cat /etc/audit/rules.d/audit.rules | grep /lib/systemd/system/docker.service &> /dev/null; then
-#     echo "-w /lib/systemd/system/docker.service -k docker" >> /etc/audit/rules.d/audit.rules
-#     echo "7. docker.service audit 설정"
-#     auditd_flag=true
-# fi
+### 5. /var/lib/docker audit 설정
+if ! command cat /etc/audit/rules.d/audit.rules | grep /var/lib/docker &> /dev/null; then
+    echo "-w /var/lib/docker -k docker" >> /etc/audit/rules.d/audit.rules
+    echo "5. /var/lib/docker audit 설정"
+    auditd_flag=true
+fi
 
-# ### 8. docker.socket audit 설정
-# if ! command cat /etc/audit/rules.d/audit.rules | grep /lib/systemd/system/docker.socket &> /dev/null; then
-#     echo "-w /lib/systemd/system/docker.socket -k docker" >> /etc/audit/rules.d/audit.rules
-#     echo "8. docker.socket audit 설정"
-#     auditd_flag=true
-# fi
 
-# ### 9. /etc/default/docker audit 설정
-# if ! command cat /etc/audit/rules.d/audit.rules | grep  /etc/default/docker &> /dev/null; then
-#     echo "-w /etc/default/docker -k docker" >> /etc/audit/rules.d/audit.rules
-#     echo "9. /etc/default/docker audit 설정"
-#     auditd_flag=true
-# fi
+### 6. /etc/docker audit 설정
+if ! command cat /etc/audit/rules.d/audit.rules | grep  /etc/docker &> /dev/null; then
+    echo "-w /etc/docker -k docker" >> /etc/audit/rules.d/audit.rules
+    echo "6. /etc/docker audit 설정"
+    auditd_flag=true
+fi
 
-# if [ "$auditd_flag" = true ]; then
-#     service auditd restart
-# fi
 
-# ### 10. default bridege를 통한 컨테이너간 네트워크 트래픽 제한
+### 7. docker.service audit 설정
+if ! command cat /etc/audit/rules.d/audit.rules | grep /lib/systemd/system/docker.service &> /dev/null; then
+    echo "-w /lib/systemd/system/docker.service -k docker" >> /etc/audit/rules.d/audit.rules
+    echo "7. docker.service audit 설정"
+    auditd_flag=true
+fi
+
+
+### 8. docker.socket audit 설정
+if ! command cat /etc/audit/rules.d/audit.rules | grep /lib/systemd/system/docker.socket &> /dev/null; then
+    echo "-w /lib/systemd/system/docker.socket -k docker" >> /etc/audit/rules.d/audit.rules
+    echo "8. docker.socket audit 설정"
+    auditd_flag=true
+fi
+
+
+### 9. /etc/default/docker audit 설정
+if ! command cat /etc/audit/rules.d/audit.rules | grep  /etc/default/docker &> /dev/null; then
+    echo "-w /etc/default/docker -k docker" >> /etc/audit/rules.d/audit.rules
+    echo "9. /etc/default/docker audit 설정"
+    auditd_flag=true
+fi
+
+if [ "$auditd_flag" = true ]; then
+    service auditd restart
+fi
+
+
+### 10. default bridege를 통한 컨테이너간 네트워크 트래픽 제한
 if ! command docker network ls --quiet | xargs docker network inspect --format '{{.Name}}: {{ .Options }}' | grep com.docker.network.bridge.enable_icc:false &> /dev/null; then
 
     service docker stop
@@ -155,12 +162,12 @@ if ! command docker network ls --quiet | xargs docker network inspect --format '
     systemctl daemon-reload
 fi
 
-# ### 11. 도커 클라이언트 인증 활성화
 
+### 11. 도커 클라이언트 인증 활성화
 # docker plugin ls 명령어 실행 결과를 변수에 저장
 # PLUGIN_LIST=$(docker plugin ls)
-
 # 플러그인 목록이 비어 있는지 확인
+
 if [[ "$(docker plugin ls)" == *"ID        NAME      DESCRIPTION   ENABLED"* ]]; then
     echo "Docker 플러그인이 설치되어 있지 않습니다."
 
@@ -179,11 +186,13 @@ if [[ "$(docker plugin ls)" == *"ID        NAME      DESCRIPTION   ENABLED"* ]];
     # service docker.service restart
 fi
 
+
 ### 12. docker.service 소유권 설정
 if [ ! "$(stat -c '%U:%G' /lib/systemd/system/docker.service)" == "root:root" ]; then
     sudo chown root:root /lib/systemd/system/docker.service
     echo docker.service 소유권 설정
 fi
+
 
 ### 13. docker.service 파일 접근권한 설정
 SERVICE_PATH=$(systemctl show -p FragmentPath docker.service | awk -F= '{print $2}')
@@ -196,6 +205,7 @@ if [ -n "$SERVICE_PATH" ]; then
     fi
 fi
 
+
 ### 14. docker.socket 소유권 설정
 SOCKET_PATH=$(systemctl show -p FragmentPath docker.socket | awk -F= '{print $2}')
 # 결과값이 있는지 확인
@@ -206,24 +216,70 @@ if [ -n "$SOCKET_PATH" ]; then
     fi
 fi
 
+
 ### 15. /etc/docker 디렉터리 소유권 설정
 if [ ! "$(stat -c '%U:%G' /etc/docker)" == "root:root" ]; then
     sudo chown root:root /etc/docker
     echo /etc/docker 디렉터리 소유권 설정
 fi
 
+
 ### 16. /etc/docker 디렉터리 접근권한 설정
 ETC_DOCKER_DIR="/etc/docker"
 if [ -d "$ETC_DOCKER_DIR" ]; then
-    PERMISSIONS=$(stat -c '%a' $ETC_DOCKER_DIR)
-    if [ "$PERMISSIONS" -gt 755 ]; then
+    if [ "$(stat -c '%a' $ETC_DOCKER_DIR)" -gt 755 ]; then
         chmod 755 $ETC_DOCKER_DIR
         echo "/etc/docker 디렉터리 접근권한 설정"
     fi
 fi
 
+
 ### 17. /var/run/docker.sock 파일 소유권 설정
 if [ ! "$(stat -c '%U:%G' /var/run/docker.sock)" == "root:docker" ]; then
     sudo chown root:docker /var/run/docker.sock
     echo /var/run/docker.sock 파일 소유권 설정
+fi
+
+
+### 18. /var/run/docker.sock 파일 접근 권한 설정
+DOCKER_SOCK="/var/run/docker.sock"
+if ls $DOCKER_SOCK &> /dev/null; then
+    if [ "$(stat -c '%a' $DOCKER_SOCK)" -gt 660 ]; then
+        sudo chmod 660 $DOCKER_SOCK
+        echo "/var/run/docker.sock 파일 접근 권한 설정"
+    fi
+fi
+
+
+### 19. daemon.json 파일 소유권 설정
+if [ ! "$(stat -c '%U:%G' /etc/docker/daemon.json)" == "root:root" ]; then
+    sudo chown root:root /etc/docker/daemon.json
+    echo daemon.json 파일 소유권 설정
+fi
+
+
+### 20. daemon.json 파일 접근권한 설정
+DAEMON_JSON="/etc/docker/daemon.json"
+if ls $DAEMON_JSON &> /dev/null; then
+    if [ "$(stat -c '%a' $DAEMON_JSON)" -gt 644 ]; then
+        sudo chmod 644 $DAEMON_JSON
+        echo "/etc/docker 디렉터리 접근권한 설정"
+    fi
+fi
+
+
+### 21. /etc/default/docker 파일 소유권 설정
+if [ ! "$(stat -c '%U:%G' /etc/default/docker)" == "root:root" ]; then
+    sudo chown root:root /etc/default/docker
+    echo /etc/default/docker 파일 소유권 설정
+fi
+
+
+### 22. /etc/default/docker 파일 접근권한 설정
+DEFAULT_DOCKER="/etc/default/docker"
+if ls $DEFAULT_DOCKER &> /dev/null; then
+    if [ "$(stat -c '%a' $DEFAULT_DOCKER)" -gt 644 ]; then
+        sudo chmod 644 $DEFAULT_DOCKER
+        echo "/etc/default/docker 파일 접근권한 설정"
+    fi
 fi
