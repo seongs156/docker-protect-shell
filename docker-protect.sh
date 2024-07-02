14f6058388ae302581edb0405cb7e748196dc927
ADMIN\USER <seongs156@gmail.com>
2024-07-02 15:03:41 +0900

    수정

--- docker-protect.sh
+++ docker-protect.sh
@@ -8,13 +8,13 @@ if [[ $EUID -ne 0 ]]; then
     abort "This must be run as root. (root 계정으로 실행해주세요.)"
 fi
 
-# echo
-# outputQuestion "
-# Docker Security Vulnerabilities Do you want to continue? (도커 보안 취약점 진행을 계속하시겠습니까?) [n/Y]"
-# read -p " " -r
-# if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-#     abort
-# fi
+echo
+outputQuestion "
+Docker Security Vulnerabilities Do you want to continue? (도커 보안 취약점 진행을 계속하시겠습니까?) [n/Y]"
+read -p " " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    abort
+fi
 
 #### Main
 
@@ -23,35 +23,35 @@ fi
 # rm -rf /var/lib/docker
 
 
-# ## 1. 도커 설치
-# if ! command rpm -qa | grep docker-ce &> /dev/null; then
-#     echo "Docker 설치중..."
-#     sudo yum install -y yum-utils
-#     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#     sudo yum install -y docker-ce docker-ce-cli containerd.io
-#     sudo systemctl enable docker
-#     sudo systemctl start docker
-#     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#     sudo chmod +x /usr/local/bin/docker-compose
-#     docker_version=$(docker version --format '{{.Server.Version}}')
-#     echo "1. Docker is install. Version: $docker_version"
-# else
-#     docker_version=$(docker version --format '{{.Server.Version}}')
-#     echo "Docker is installed. Version: $docker_version"
-# fi
+## 1. 도커 설치
+if ! command rpm -qa | grep docker-ce &> /dev/null; then
+    echo "Docker 설치중..."
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker_version=$(docker version --format '{{.Server.Version}}')
+    echo "1. Docker is install. Version: $docker_version"
+else
+    docker_version=$(docker version --format '{{.Server.Version}}')
+    echo "Docker is installed. Version: $docker_version"
+fi
 
 
 
-# ## 2. 도커 최신버전 패치
-# docker_version=$(docker version --format '{{.Server.Version}}')
-# rpm_docker_version=$(rpm -qa | grep docker-ce | awk -F'-' '/docker-ce-[0-9]/ {print $3}')
+## 2. 도커 최신버전 패치
+docker_version=$(docker version --format '{{.Server.Version}}')
+rpm_docker_version=$(rpm -qa | grep docker-ce | awk -F'-' '/docker-ce-[0-9]/ {print $3}')
 
-# if [ "$docker_version" != "$rpm_docker_version" ]; then
-#     sudo yum update docker-ce docker-ce-cli containerd.io
-#     sudo systemctl restart docker
-#     docker_version=$(docker version --format '{{.Server.Version}}')
-#     echo "2. Docker Update Version: $docker_version"
-# fi
+if [ "$docker_version" != "$rpm_docker_version" ]; then
+    sudo yum update docker-ce docker-ce-cli containerd.io
+    sudo systemctl restart docker
+    docker_version=$(docker version --format '{{.Server.Version}}')
+    echo "2. Docker Update Version: $docker_version"
+fi
 
 
 
@@ -77,6 +77,7 @@ fi
 ### 4. Docker daemon audit 설정
 auditd_flag=false
 if ! command cat /etc/audit/rules.d/audit.rules | grep /usr/bin/docker &> /dev/null; then
+    echo "-w /usr/bin/docker" >> /etc/audit/rules.d/audit.rules
     echo "Docker daemon audit 설정"
     auditd_flag=true
 fi
@@ -84,6 +85,7 @@ fi
 
 ### 5. /var/lib/docker audit 설정
 if ! command cat /etc/audit/rules.d/audit.rules | grep /var/lib/docker &> /dev/null; then
+    echo "-w /var/lib/docker -k docker" >> /etc/audit/rules.d/audit.rules
     echo "/var/lib/docker audit 설정"
     auditd_flag=true
 fi
@@ -91,6 +93,7 @@ fi
 
 ### 6. /etc/docker audit 설정
 if ! command cat /etc/audit/rules.d/audit.rules | grep  /etc/docker &> /dev/null; then
+    echo "-w /etc/docker -k docker" >> /etc/audit/rules.d/audit.rules
     echo "/etc/docker audit 설정"
     auditd_flag=true
 fi
@@ -98,6 +101,7 @@ fi
 
 ### 7. docker.service audit 설정
 if ! command cat /etc/audit/rules.d/audit.rules | grep /lib/systemd/system/docker.service &> /dev/null; then
+    echo "-w /lib/systemd/system/docker.service -k docker" >> /etc/audit/rules.d/audit.rules
     echo "docker.service audit 설정"
     auditd_flag=true
 fi
@@ -105,6 +109,7 @@ fi
 
 ### 8. docker.socket audit 설정
 if ! command cat /etc/audit/rules.d/audit.rules | grep /lib/systemd/system/docker.socket &> /dev/null; then
+    echo "-w /lib/systemd/system/docker.socket -k docker" >> /etc/audit/rules.d/audit.rules
     echo "docker.socket audit 설정"
     auditd_flag=true
 fi
@@ -112,6 +117,7 @@ fi
 
 ### 9. /etc/default/docker audit 설정
 if ! command cat /etc/audit/rules.d/audit.rules | grep  /etc/default/docker &> /dev/null; then
+    echo "-w /etc/default/docker -k docker" >> /etc/audit/rules.d/audit.rules
     echo "/etc/default/docker audit 설정"
     auditd_flag=true
 fi
@@ -244,16 +250,17 @@ if ls $DOCKER_SOCK &> /dev/null; then
     fi
 fi
 
-
+DAEMON_JSON="/etc/docker/daemon.json"
 ### 19. daemon.json 파일 소유권 설정
-if [ ! "$(stat -c '%U:%G' /etc/docker/daemon.json)" == "root:root" ]; then
-    sudo chown root:root /etc/docker/daemon.json
-    echo daemon.json 파일 소유권 설정
+if ls $DAEMON_JSON &> /dev/null; then
+    if [ ! "$(stat -c '%U:%G' $DAEMON_JSON)" == "root:root" ]; then
+        sudo chown root:root $DAEMON_JSON
+        echo daemon.json 파일 소유권 설정
+    fi
 fi
 
 
 ### 20. daemon.json 파일 접근권한 설정
-DAEMON_JSON="/etc/docker/daemon.json"
 if ls $DAEMON_JSON &> /dev/null; then
     if [ "$(stat -c '%a' $DAEMON_JSON)" -gt 644 ]; then
         sudo chmod 644 $DAEMON_JSON
